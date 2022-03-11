1f7c21b0254eb86625f2da5bbe888967fcaed763
Julia <julia.gray30@gmail.com>
2022-03-11 20:12:58 +0000

    file system set up, packages installed and notes added

---
+++ index.js
@@ -0,0 +1,50 @@
+
+
+
+
+
+// Mysql2
+// Inquirer
+// Console table package 
+
+
+
+// Inquirer:
+// 1. View all departments
+// 2. View all roles
+// 3. View all employees
+// 4. View employees by manager
+// 5. View employees by department
+// 6. View total utilized budget of a department ()
+// 5. Add a department
+// 6. Add a role
+// 7. Add an employee
+// 8. Update an employee role
+// 9. Remove 
+    // 9a. Delete department
+    // 9b. Delete role
+    // 9c. Delete employee
+
+// View all departments = formatted table > Department Names // Department IDs
+
+// View all roles = table > Job title // Role ID // Departments the role belongs to // Salary for the role 
+
+// View all employees = table > Employee ID // First name // Last name // Job title // Department // Salary // Managers that employee reports to
+
+// Add a department = Add name of department to the database 
+
+// Add a role = Add name, salary, department for the role to the database. 
+
+// Add an employee = Add employees first name, last name, role, manager. 
+
+// Update employee role = Select employee and update role. 
+
+// Update employee managers = Select employee and update manager
+
+// View employees by manager
+
+// View employees by department
+
+// Delete departments, roles, and employees.
+
+// View the total utilized budget of a department = the combined salaries of all employees in that department.
