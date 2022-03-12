bfae0d7ede0ea895e0bda4fb4de07bd835b86060
Julia <julia.gray30@gmail.com>
2022-03-12 09:40:29 +0000

    Inquirer built with all questions

--- index.js
+++ index.js
@@ -1,43 +1,126 @@
+const inquirer = require("inquirer");
+// const mysql = require("mysql2");
+// Console table package
 
+let mainMenuQuestions = [
+  {
+    prefix: "What would you like to do today?",
+    type: "list",
+    name: "menuOption",
+    // message: "What would you like to do today?",
+    choices: [
+      "View all departments",
+      "View all roles",
+      "View employees",
+      "View budget",
+      "Add a department",
+      "Add a role",
+      "Add an employee",
+      "Update an employee role",
+      "Remove",
+          ],
+  },
+  {
+    type: "list",
+    name: "viewEmployees",
+    message: "How would you like to view the employees?",
+    choices: ["View all", "View by manager", "View by department"],
+    when: (answers) => answers.menuOption === "View employees",
+  },
+  {
+    type: "list",
+    name: "removeEmployees",
+    message: "What do you want to Delete?",
+    choices: ["Delete department", "Delete role", "Delete employee"],
+    when: (answers) => answers.menuOption === "Remove",
+  },
+];
 
+async function askQuestions() {
+  const mainMenuOptions = await inquirer.prompt(mainMenuQuestions);
+  switch (mainMenuOptions.menuOption) {
+    case "View all departments":
+      console.log("View all departments");
+      break;
+    case "View all roles":
+      console.log("View all roles");
+      break;
+    case "View employees":
+        switch (mainMenuOptions.viewEmployees) {
+            case "View all":
+              console.log("View all selected");
+              break;
+            case "View by manager":
+              console.log("View by man");
+              break;
+            case "View by department":
+              console.log("View by dep");
+              break;
+            default:
+              console.log("Try again");
+          }
+      break;
+    case "View budget":
+      console.log("View budg");
+      break;
+    case "Add a department":
+      console.log("Add department");
+      break;
+    case "Add a role":
+      console.log("Add a role");
+      break;
+    case "Add an employee":
+      console.log("Add an employee");
+      break;
+    case "Update an employee role":
+      console.log("Update role");
+      break;
+    case "Remove":
+      switch (mainMenuOptions.removeEmployees) {
+        case "Delete department":
+          console.log("Del dep");
+          break;
+        case "Delete role":
+          console.log("Del role");
+          break;
+        case "Delete employee":
+          console.log("del emp");
+          break;
+    default:
+      console.log("Something went wrong")
+  }
+  }}
 
-
-
-// Mysql2
-// Inquirer
-// Console table package 
-
-
+askQuestions();
 
 // Inquirer:
 // 1. View all departments
 // 2. View all roles
 // 3. View all employees
-// 4. View employees by manager
-// 5. View employees by department
-// 6. View total utilized budget of a department ()
-// 5. Add a department
-// 6. Add a role
-// 7. Add an employee
-// 8. Update an employee role
-// 9. Remove 
-    // 9a. Delete department
-    // 9b. Delete role
-    // 9c. Delete employee
+//  3a. View employees by manager
+//  3b. View employees by department
+// 4. Add a department
+// 5. Add a role
+// 6. Add an employee
+// 7. Update an employee role
+// 8. Remove
+// 8a. Delete department
+// 8b. Delete role
+// 8c. Delete employee
 
 // View all departments = formatted table > Department Names // Department IDs
 
-// View all roles = table > Job title // Role ID // Departments the role belongs to // Salary for the role 
+// View all roles = table > Job title // Role ID // Departments the role belongs to // Salary for the role
 
 // View all employees = table > Employee ID // First name // Last name // Job title // Department // Salary // Managers that employee reports to
 
-// Add a department = Add name of department to the database 
+// Add a department = Add name of department to the database
 
-// Add a role = Add name, salary, department for the role to the database. 
+// Add a role = Add name, salary, department for the role to the database.
 
-// Add an employee = Add employees first name, last name, role, manager. 
+// Add an employee = Add employees first name, last name, role, manager.
 
-// Update employee role = Select employee and update role. 
+// Update employee role = Select employee and update role.
 
 // Update employee managers = Select employee and update manager
 
